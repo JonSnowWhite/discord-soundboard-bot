73ebe790c6773fbc8e9f6a99b12db293733b1e98
JonSnowWhite <salkin99@gmx.de>
2017-11-06 22:18:07 +0100

    Added reaction responses, a better logging function and the info and change_activity command

--- cogs/sound_cog.py
+++ cogs/sound_cog.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+from ext_module import ExtModule
 import os
 import asyncio
 from random import randint
@@ -20,7 +21,7 @@ class SoundboardCog:
         self.folder = folder
         self.bot = bot
         self.log_channel_id = log_channel_id
-        self.log_channel = None             # will be assigned later
+        self.send_log = None             # will be assigned later
         self.sound_list = SoundboardCog._load_songs(self.folder)
 
     @staticmethod
@@ -38,11 +39,13 @@ class SoundboardCog:
         return sound_list
 
     async def on_ready(self):
-        self.log_channel = self.bot.get_channel(self.log_channel_id)
+        self.send_log = ExtModule.get_send_log(self)
 
-    @commands.command(description='name(optional) || Plays the sound with the given name. Ignores upper/lowercase.'
+    @commands.command(name='play',
+                      description='name(optional) || Plays the sound with the given name. Ignores upper/lowercase.'
                                   ' If none is found or none is given the sound is chosen randomly.'
                                   ' Requires you to be in a voice channel!')
+    @ExtModule.reaction_respond
     async def play(self, ctx: commands.Context, name=''):
         """This command plays the sound with the given name (random if no name given or name is write falsely)
 
@@ -53,27 +56,31 @@ class SoundboardCog:
         try:
             voice_channel = ctx.author.voice.channel
         except AttributeError:
-            return await ctx.send(content='To use this command you have to be connected to a voice channel!')
+            await ctx.send(content='To use this command you have to be connected to a voice channel!')
+            raise discord.DiscordException
         if name != '' and name.lower() not in self.sound_list:
-            await ctx.send('This files does\'t exist. Check the help command for more info!')
+            try:
+                await ctx.send('This files does not exist. Check the help command for more info!')
+            except discord.DiscordException:
+                pass
             name = self.sound_list[randint(1, len(self.sound_list)) - 1]
         elif name == '':
             name = self.sound_list[randint(1, len(self.sound_list)) - 1]
         try:
             vc = await voice_channel.connect()
         except discord.ClientException:
-            return await ctx.send('I am already playing in a voice channel.'
-                                  ' Please try again later or stop me with the stop command!')
+            await ctx.send('I am already playing in a voice channel.'
+                           ' Please try again later or stop me with the stop command!')
+            raise discord.DiscordException
         vc.play(discord.FFmpegPCMAudio(self.folder + '/' + name.lower() + '.mp3'),
                 after=lambda e: SoundboardCog.disconnector(vc, self.bot))
-        try:
-            await self.log_channel.send('Playing: ' + name)
-        except AttributeError:
-            pass
+        await self.send_log('Playing: ' + name)
 
-    @commands.command(aliases=['halt'],
+    @commands.command(name='stop',
+                      aliases=['halt'],
                       description='The bot will stop playing a sound and leave the current voice channel.'
                                   'Requires you to be in the same voice channel as the bot!')
+    @ExtModule.reaction_respond
     async def stop(self, ctx: commands.Context):
         """This function stops the bot playing a sound and makes it leave the current voice channel.
          Args:
@@ -83,20 +90,22 @@ class SoundboardCog:
             if ctx.author.voice.channel == connection.channel:
                 return await connection.disconnect()
 
-    @commands.command(aliases=['sounds'], description='Prints a list of all sounds on the soundboard.')
+    @commands.command(name='soundlist',
+                      aliases=['sounds'], description='Prints a list of all sounds on the soundboard.')
+    @ExtModule.reaction_respond
     async def soundlist(self, ctx: commands.Context):
         """This function prints a list of all the sounds on the Soundboard to the channel/user where it was requested.
         Args:
             ctx: The context of the command, which is mandatory in rewrite (commands.Context)
             """
-        _sound_string = 'List of all sounds:'
+        _sound_string = 'List of all sounds (command format !play [soundname]\'):'
         for sound in self.sound_list:
             if len(_sound_string) + 1 + len(sound) > 1800:
                 await ctx.channel.send(_sound_string)
                 _sound_string = ''
             else:
                 _sound_string = _sound_string + '\n' + sound
-        return await ctx.channel.send(_sound_string)
+        await ctx.channel.send(_sound_string)
 
     @staticmethod
     def disconnector(voice_client: discord.VoiceClient, bot: commands.Bot):
--- ext_module.py
+++ ext_module.py
@@ -1,5 +1,6 @@
 """This module contains additional functions, checks and classes used int the Cogs"""
 from discord.ext import commands
+import discord
 
 
 class ExtModule:
@@ -12,7 +13,7 @@ class ExtModule:
         Returns:
             Boolean: True or False, wether the invoker of a command fulfills the predicate
             """
-        return commands.is_owner()
+        return await ctx.bot.is_owner(ctx.author)
 
     @staticmethod
     def is_admin():
@@ -35,3 +36,78 @@ class ExtModule:
             _aliases = _aliases + str(alias) + ', '
         _aliases = '(' + _aliases[:-2] + ')'
         return _name + ' ' + _aliases + ': ' + _description
+
+    @staticmethod
+    def _send_log_generator(log_channel):
+        """Returns a send_log_channel function
+        Args:
+            log_channel: The destination send_log() will send its logs to, can be a user and text channel
+            (inheriting class of both discord.Snowflake and discord.Messageable)
+        Returns:
+              send_log(): A function which sends the given message to the log_channel
+            """
+        async def send_log(message: str):
+            """Sends the given message to the log_channel found in the closure. Using this function allows for easy logging
+            Args:
+                message: The message to be send
+                """
+            try:
+                x = await log_channel.send(message)
+                return x
+                # return await log_channel.send(message)
+            except discord.Forbidden:
+                print('Permissions for log channel with id ' + str(log_channel.id) + ' missing')
+            except discord.HTTPException:
+                print('Sending a message to ' + log_channel.name + ' failed.')
+        return send_log
+
+    @staticmethod
+    def get_send_log(cog):
+        _log_channel = cog.bot.get_channel(cog.log_channel_id)
+        if _log_channel is None:
+            _log_channel = cog.bot.get_user(cog.log_channel_id)
+        if _log_channel is None:
+            print('ID for log_channel of cog ' + cog.__class__.__name__ + 'is not valid')
+            return lambda x: print('Could not print help message' + x +
+                                   'to log_channel of Cog' + cog.__class__.__name__ +
+                                   'due to an invalid ID.')
+        else:
+            return ExtModule._send_log_generator(_log_channel)
+
+    @staticmethod
+    def reaction_respond(fun):
+        """This decorator will catch any Exceptions commands may give and give different discord.Reactions considering
+        if any (and if yes, which?) Exceptions occurred. The command will not have to handle feedback to the User,
+        the User will see a simple reply through reactions and the bot does not need write permissions for commands,
+        which do not base on text response.
+        """
+        async def responded_function(self, ctx: commands.Context, *args):
+            try:
+                await fun(self, ctx, *args)
+            except PmForbidden:
+                await ctx.message.add_reaction('\U0001F1F2')
+                await ctx.message.add_reaction('\U0001F1F5')
+                await ctx.message.add_reaction('\U0000274C')
+                await ctx.message.add_reaction('\U0001F1F5')
+                await ctx.message.add_reaction('\U0001F1EA')
+                await ctx.message.add_reaction('\U0001F1F7')
+                await ctx.message.add_reaction('\U0001F1F2')
+                await ctx.message.add_reaction('\U0001F1F8')
+            except discord.Forbidden:
+                await ctx.message.add_reaction('\U0000274C')
+                await ctx.message.add_reaction('\U0001F1F5')
+                await ctx.message.add_reaction('\U0001F1EA')
+                await ctx.message.add_reaction('\U0001F1F7')
+                await ctx.message.add_reaction('\U0001F1F2')
+                await ctx.message.add_reaction('\U0001F1F8')
+            except Exception:
+                await ctx.message.add_reaction('\U0000274C')
+            else:
+                await ctx.message.add_reaction('\U00002705')
+        return responded_function
+
+
+class PmForbidden(discord.Forbidden):
+    """Exception that will be thrown manually after a pm to a user failed with discord.Forbidden
+    """
+    pass
