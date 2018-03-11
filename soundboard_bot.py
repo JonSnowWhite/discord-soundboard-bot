356f5eddb2917763017807b196be4d32b92f683c
JonSnowWhite <salkin99@gmx.de>
2018-03-11 12:14:16 +0100

    Update soundboard_bot.py

--- soundboard_bot.py
+++ soundboard_bot.py
@@ -9,10 +9,11 @@ command_prefix = '!'
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(command_prefix))
 sound_folder = '~~your folder~~'
 log_channel_id = int('~~your log channel id~~')
+standard_activity = command_prefix + 'help'
 tag_dict={'tag1': ['name1', 'name2'],  # this dict saves the tags and their sounds. Be sure to use lists also when a tag has only 1 name
           'tag2': ['name1', 'name2', 'name3']}
 bot.add_cog(SoundboardCog(bot=bot, folder=sound_folder, log_channel_id=log_channel_id, tag_dict=tag_dict))
-bot.add_cog(AdminCog(bot=bot, log_channel_id=log_channel_id))
+bot.add_cog(AdminCog(bot=bot, log_channel_id=log_channel_id, activity =standard_activity))
 bot.add_cog(UserCog(bot=bot, log_channel_id=log_channel_id))
 
 @bot.event
